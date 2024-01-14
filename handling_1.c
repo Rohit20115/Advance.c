fb335fe75e99dfd19717fb6c435cf9ebf231f72d
Rohit sahu <143478312+Rohit20115@users.noreply.github.com>
2024-01-14 13:25:31 +0530

    Add files via upload

---
+++ handling_1.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+main()
+{
+
+        FILE *fp;
+        int number,quantity,i;
+        float price,value;
+        char item[10],filename[10];
+
+        printf("input file name \n");
+        scanf("%s",filename);
+
+        fp=fopen(filename,"w");
+        printf("input inventory data \n\n");
+        printf("item name number price quantity \n");
+        for(i=1;i<=3;i++)
+        {
+                fscanf(stdin,"%s %d %f %d ",item, &number, &price, &quantity);
+                fprintf(fp,"%s %d %2f %d",item, number, price, quantity);
+        }
+        fclose(fp);
+
+        fprintf(stdout,"r");
+
+        fp=fopen(filename,"r");
+
+        printf("item name   number  price   quantity    valum\n");
+        for(i=1;i<=3;i++)
+        {
+                fscanf(fp, "%s %d %f %d",item,&number,&price,&quantity);
+                value=price*quantity;
+                fprintf(stdout,"%-8s %7d %8d %11.2f\n",item,number,price,quantity,value);
+        }
+        fclose(fp);
+
+}
